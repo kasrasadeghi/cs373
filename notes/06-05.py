d7047d09399e1a13f0c599ab1b0ea846e41e4e85
Glenn Downing <downing@cs.utexas.edu>
2017-06-05 18:19:26 -0500

    another commit

---
+++ notes/06-05.py
@@ -0,0 +1,38 @@
+# ----------
+# Mon, 5 Jun
+# ----------
+
+"""
+three styles of input:
+
+1. tell me up front
+2. give me some sort of sentinel at the end
+3. expect the read to eventually fail (end-of-file)
+
+three parts to the solution:
+
+1. kernel
+2. run harness
+3. test harness
+
+kernel can NOT know where input is coming from or where output is going to
+
+Python, JavaScript have "duck" typing
+
+REPL: read, eval, print, loop
+"""
+
+# ---------
+# Questions
+# ---------
+
+"""
+What does range(n) return?
+What type did the test harness to use to simulate stdin? stdout?
+What's the purpose of .gitignore?
+What does autopep8 do?
+What does mypy do?
+What does pylint do?
+What does pydoc do?
+What does coverage do?
+"""
