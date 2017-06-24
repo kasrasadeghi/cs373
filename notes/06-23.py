52e9864558c9d308d2b82756d246db7a791d5314
Glenn Downing <downing@cs.utexas.edu>
2017-06-23 22:36:35 -0500

    another commit

--- notes/06-23.py
+++ notes/06-23.py
@@ -2,3 +2,46 @@
 # Fri, 23 Jun
 # -----------
 
+p = f
+print(p is f) # True
+
+x = range(2, 5)
+print(type(x)) # range
+v = next(x)    # not ok
+p = iter(x)
+print(x is p)  # False
+print(type(p)) # range iterator
+q = iter(p)
+print(p is q)  # True
+v = next(p)
+print(v)       # 2
+v = next(p)
+print(v)       # 3
+v = next(p)
+print(v)       # 4
+v = next(p)    # raise StopIteration
+
+class range_iterator :
+    def __init__ (self, b, e) :
+        self.b = b
+        self.e = e
+
+    def __iter__ (self) :
+        return self
+
+    def __next__ (self) :
+        if self.b == self.e :
+            raise StopIteration
+        b = self.b
+        self.b += 1
+        return b
+
+def range_iterator (b, e) :
+    while b != e :
+        yield b
+        b += 1
+
+"""
+three tokens: =, *, **
+two contexts: function call, function definition
+"""
