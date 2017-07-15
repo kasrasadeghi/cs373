91704e60b78413a3f01e6cbf75259a6f88a07845
Glenn Downing <downing@cs.utexas.edu>
2017-07-15 14:15:31 -0500

    another commit

--- notes/07-14.py
+++ notes/07-14.py
@@ -16,3 +16,15 @@ class T {
         else
             x = new B();
         x.f();
+
+# ---------
+# Questions
+# ---------
+
+"""
+What are the multiplicities of Customer, Rental, and Movie in Fowler's example?
+The refactoring that everyone thought of in yesterday's exercise is called "Replace Type Code with Subclasses". What was the downside of that refactoring for that problem?
+The refactoring that Fowlder did is called "Replace Type Code with State / Strategy". How is that different?
+Did that refactoring completely remove the switch statement or just move it?
+If only moved, what's the benefit of the refactoring?
+"""
