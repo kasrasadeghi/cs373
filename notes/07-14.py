ddf105adabbf48e64c4cdcba3ccb7a10a857a901
Glenn Downing <downing@cs.utexas.edu>
2017-07-15 09:39:22 -0500

    another commit

--- notes/07-14.py
+++ notes/07-14.py
@@ -2,3 +2,17 @@
 # Fri, 14 Jul
 # -----------
 
+class A {
+    void f () {...}}
+
+class B extends A {
+    void f () {...}};
+
+class T {
+    public static void main (...) {
+        A x;
+        if (...)
+            x = new A();
+        else
+            x = new B();
+        x.f();
