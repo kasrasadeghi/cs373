33364f747c7ea603d56a0263cdbb3e21e0f07fb3
Glenn Downing <downing@cs.utexas.edu>
2017-07-17 12:14:03 -0500

    another commit

--- examples/MethodOverriding.java
+++ examples/MethodOverriding.java
@@ -7,14 +7,20 @@ class A {
         return "A.f";}
 
     public String g (int i) {
-        return "A.g";}}
+        return "A.g";}
+
+    public String h (long l) {
+        return "A.h";}}
 
 class B extends A {
     public String f (int i) {
         return "B.f";}
 
     public String g (long l) {
-        return "B.g";}}
+        return "B.g";}
+
+    public String h (int i) {
+        return "B.h";}}
 
 final class MethodOverriding {
     public static void main (String[] args) {
@@ -25,11 +31,15 @@ final class MethodOverriding {
         assert x.f(2)  == "B.f";
         assert x.g(2)  == "A.g";
         assert x.g(2L) == "B.g";
+        assert x.h(2)  == "B.h";
+        assert x.h(2L) == "A.h";
 
         A y = new B();
 
         assert y.f(2)  == "B.f";
         assert y.g(2)  == "A.g";
 //      assert y.g(2L) == "A.g"; // error: incompatible types: possible lossy conversion from long to int
+        assert y.h(2)  == "A.h";
+        assert y.h(2L) == "A.h";
 
         System.out.println("Done.");}}
